6582b49372484c0259ca630430adae45a39ccb6b
Yousef <ywalotaibi2010@gmail.com>
2026-08-23 16:52:14 +0300

    Make functions that fetch & process weather data
    Make a function that fetches data using the visual crossings weather
    api and a function that processes that data and gives back only the
    needed data.

---
+++ data.js
@@ -0,0 +1,40 @@
+const weatherApiKey = "78VXK83JGHH3HA9V4EBR7AQ2V"
+
+export async function fetchWeatherData(location) {
+    try {
+        const date = new Date();
+        const formattedDate = `${date.getFullYear()}-${date.getMonth()}-${date.getDate()}`
+        const weatherResponse = await fetch(`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}/${formattedDate}?key=${weatherApiKey}`)
+        const weatherData = await weatherResponse.json()
+        console.log(weatherData)
+        return weatherData
+    }
+    catch (error) {
+        throw new Error(error)
+    }
+}
+
+export function processWeatherData(data) {
+    const address = data.address
+    const maxTemperature = data.days[0].tempmax
+    const minTemperature = data.days[0].tempmin
+    const temperature = data.days[0].temp
+    const conditions = data.days[0].conditions
+    const humidity = data.days[0].humidity
+    const precipitation = data.days[0].precipitation
+    const icon = data.days[0].icon
+    return {
+        address,
+        maxTemperature,
+        minTemperature,
+        temperature,
+        conditions,
+        icon,
+    }
+}
+// For Testing:
+// (async () => {
+//     const weatherData = await fetchWeatherData('Saudi Arabia')
+//     const processedData = processWeatherData(weatherData)
+//     console.log(processedData)
+// })()
