4223e81540f7bedc95103151c660f27af86d6b95
Yousef <ywalotaibi2010@gmail.com>
2026-09-12 13:30:30 +0300

    Move test object to a seperate file and import it
    Now the code can easily be read cuz the test object isn't making the
    DOMController.js file full of meaningless lines that don't contribute
    to anything other than being a sample of data.

--- DOMController.js
+++ DOMController.js
@@ -3,6 +3,7 @@ import { tempUnits } from "./temp-units.js"
 import { validateLocationInput } from "./components/location-input.js"
 import {startLoadingComponent, stopLoadingComponent, showFetchingFailed} from './components/loading.js'
 import { populateHoursSlider } from "./components/hours-slider.js"
+import testObject from "./test-object.js"
 const getWeatherForm = document.querySelector('form')
 getWeatherForm.addEventListener('submit', async (event)=>{
     event.preventDefault()
@@ -27,137 +28,6 @@ getWeatherForm.addEventListener('submit', async (event)=>{
 })
 
 const isTesting = false
-
-const testObject = {
-    address: "Saudi Arabia",
-    minTemp: 92,
-    maxTemp: 105,
-    temp: 97,
-    conditions: "weather is pretty nice yo!",
-    icon: "partly-cloudy-day",
-    hours: [
-        {
-            "datetime": "00:00:00",
-            "temp": 96.7,
-            "icon": "clear-night",
-        },
-        {
-            "datetime": "01:00:00",
-            "temp": 93.1,
-            "icon": "clear-night",
-        },
-        {
-            "datetime": "02:00:00",
-            "temp": 93.1,
-            "icon": "clear-night",
-        },
-        {
-            "datetime": "03:00:00",
-            "temp": 91.3,
-            "icon": "clear-night",
-        },
-        {
-            "datetime": "04:00:00",
-            "temp": 87.7,
-            "icon": "clear-night",
-        },
-        {
-            "datetime": "05:00:00",
-            "temp": 87.7,
-            "icon": "clear-night",
-        },
-        {
-            "datetime": "06:00:00",
-            "temp": 87.7,
-            "icon": "clear-day",
-        },
-        {
-            "datetime": "07:00:00",
-            "temp": 91.3,
-            "icon": "clear-day",
-        },
-        {
-            "datetime": "08:00:00",
-            "temp": 98.5,
-            "icon": "clear-day",
-        },
-        {
-            "datetime": "09:00:00",
-            "temp": 107.5,
-            "icon": "clear-day",
-        },
-        {
-            "datetime": "10:00:00",
-            "temp": 109.3,
-            "icon": "clear-day",
-        },
-        {
-            "datetime": "11:00:00",
-            "temp": 112.9,
-            "icon": "partly-cloudy-day",
-        },
-        {
-            "datetime": "12:00:00",
-            "temp": 112.9,
-            "icon": "partly-cloudy-day",
-        },
-        {
-            "datetime": "13:00:00",
-            "temp": 111.1,
-            "icon": "partly-cloudy-day",
-        },
-        {
-            "datetime": "14:00:00",
-            "temp": 111.1,
-            "icon": "partly-cloudy-day",
-        },
-        {
-            "datetime": "15:00:00",
-            "temp": 109.3,
-            "icon": "partly-cloudy-day",
-        },
-        {
-            "datetime": "16:00:00",
-            "temp": 111.1,
-            "icon": "partly-cloudy-day",
-        },
-        {
-            "datetime": "17:00:00",
-            "temp": 109.3,
-            "icon": "partly-cloudy-day",
-        },
-        {
-            "datetime": "18:00:00",
-            "temp": 107.5,
-            "icon": "partly-cloudy-day",
-        },
-        {
-            "datetime": "19:00:00",
-            "temp": 105.7,
-            "icon": "partly-cloudy-night",
-        },
-        {
-            "datetime": "20:00:00",
-            "temp": 105.7,
-            "icon": "partly-cloudy-night",
-        },
-        {
-            "datetime": "21:00:00",
-            "temp": 100.3,
-            "icon": "partly-cloudy-night",
-        },
-        {
-            "datetime": "22:00:00",
-            "temp": 100.3,
-            "icon": "partly-cloudy-night",
-        },
-        {
-            "datetime": "23:00:00",
-            "temp": 98.5,
-            "icon": "partly-cloudy-night",
-        }
-    ]
-}
 
 let tempUnit = tempUnits.farenheit
 
